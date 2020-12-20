aa4de4201bc6ebe73c1d19a02ccd2254c1c12aad
Leonardo T. Fernandes <leo.80@protonmail.com>
2020-12-20 09:31:11 -0300

    initial typescript configuration: partial but functional

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
-const { Telegraf } = require('telegraf');
+import { Telegraf } from 'telegraf';
 const { markdown, HTML } = require('telegraf/extra');
 // const fetch = require('node-fetch');
 
-const express = require('express');
-const moment = require('moment');
+import express, { Request, Response } from 'express';
+import moment from 'moment';
 
 const getCurrency = require('./utils/getCurrency'); 
 const formatValue = require('./utils/formatValue');
@@ -14,27 +14,18 @@ require('dotenv').config();
 const app = express();
 
 const port = process.env.PORT || 3000
-app.get('/', (request, response) => response.send('App is running!'));
+app.get('/', (request: Request, response: Response) => response.send('App is running!'));
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-let dateNow = moment().locale('pt-br').format('LLLL');
-
 const dollarNow = async () => {
+    const dateNow = moment(). locale('pt-br').format('LLLL');
     const response = await getCurrency();
-
-    // little hack to avoid heroku iddling from stop the bot from responding
-    // const idlePreventResponse = await fetch(
-    //     'https://my-currency-telegram-bot.herokuapp.com'
-    // );
-    // console.log(idlePreventResponse.status);
-
-    // fetch('https://my-currency-telegram-bot.herokuapp.com/')
-    //     .then(idlePreventResponse => console.log(idlePreventResponse.status))
 
     return `O dólar hoje, ${dateNow}, está cotado em *${formatValue(response.val)}*`;
 }
 
-const bot = new Telegraf(process.env.TELEGRAM_API_KEY);
+// @ts-ignore
+const bot = new Telegraf(process.env.TELEGRAM_API_KEY); 
 
 bot.use(sanitizeUserInput);
 
